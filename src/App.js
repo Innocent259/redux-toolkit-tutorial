d1382d7c6f5c624a74997b6fc5e8968dd7a379ec
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-07-23 20:00:55 +0200

    Finish all calculation fanctionalities

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { calculateTotal } from "./features/cart/cardSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch()
+  const { cartItems } = useSelector((store) => store.cart)
+  useEffect(() => {
+    dispatch(calculateTotal())
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cartItems])
+
   return (
     <>
       <Navbar />
