c19ca7d334f9914b6bb96b66dc05da794ba4703d
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-07-24 07:23:46 +0200

    Use another Modal to finish the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import Modal from "./components/Modal";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import { calculateTotal } from "./features/cart/cardSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 
+
 function App() {
   const dispatch = useDispatch()
   const { cartItems } = useSelector((store) => store.cart)
+  const { isOpen } = useSelector((store) => store.modal)
+
   useEffect(() => {
     dispatch(calculateTotal())
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -14,6 +18,7 @@ function App() {
 
   return (
     <>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </>
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducers from './features/cart/cardSlice';
+import modalReducers from './features/modal/modalSlice'
 
 export const store = configureStore({
     reducer: {
         cart: cartReducers,
+        modal: modalReducers,
     }
 })
